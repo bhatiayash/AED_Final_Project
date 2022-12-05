2b39b5959c41bfafff307673eb4e56a55f9c477c
AkashChhabria <113483019+AkashChhabria@users.noreply.github.com>
2022-12-05 06:26:50 -0500

    added work/sales/program/booking queue

---
+++ GymManagementSystem/src/Business/WorkQueue/ProductAvailabilityQueue.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.WorkQueue;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author akash
+ */
+public class ProductAvailabilityQueue {
+    private ArrayList<ProductAvailabilityQueue> productAvailabilityList;
+    
+    public ProductAvailabilityQueue(){
+        this.productAvailabilityList = productAvailabilityList;
+    }
+
+    public ArrayList<ProductAvailabilityQueue> getProductAvailabilityList() {
+        return productAvailabilityList;
+    }
+
+    public void setProductAvailabilityList(ArrayList<ProductAvailabilityQueue> productAvailabilityList) {
+        this.productAvailabilityList = productAvailabilityList;
+    } 
+}
