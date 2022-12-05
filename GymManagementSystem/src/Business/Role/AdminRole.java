37109f1f02b1c0bdf8cea25d6678fe36b0dfffe6
bhatiayash <113397515+bhatiayash@users.noreply.github.com>
2022-12-05 06:59:27 -0500

    defined all the roles

---
+++ GymManagementSystem/src/Business/Role/AdminRole.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+
+
+/**
+ *
+ * @author yashbhatia
+ */
+public class AdminRole extends Role{
+
+   
+}
---
+++ GymManagementSystem/src/Business/Role/SystemAdminRole.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+
+
+/**
+ *
+ * @author yashbhatia
+ */
+public class SystemAdminRole extends Role{
+
+
+    
+}
