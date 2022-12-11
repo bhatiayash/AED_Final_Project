26586a908d263b93ca73f99930983f6ed137b277
Keshni21 <113250552+Keshni21@users.noreply.github.com>
2022-12-11 05:44:24 -0500

    UI changes

--- GymManagementSystem/src/UI/Customer/CancelJPanel.java
+++ GymManagementSystem/src/UI/Customer/CancelJPanel.java
@@ -35,6 +35,8 @@ public class CancelJPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        setBackground(new java.awt.Color(0, 102, 102));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
